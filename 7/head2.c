3edf95b75a73b50c04593f107e1229b8402213bb
tomotargz <nakamuratomota@gmail.com>
2021-08-25 23:58:09 +0900

    Add head commands

---
+++ 7/head2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void do_head(FILE *f, long nlines);
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s n\n", argv[0]);
+        exit(1);
+    }
+    long nlines = atol(argv[1]);
+    if (argc == 2) {
+        do_head(stdin, nlines);
+    } else {
+        for (int i = 2; i < argc; ++i) {
+            FILE *f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            fclose(f);
+        }
+    }
+    exit(0);
+}
+
+static void do_head(FILE *f, long nlines) {
+    int c;
+    if (nlines <= 0) return;
+    while ((c = getc(f)) != EOF) {
+        if (putchar(c) < 0) exit(1);
+        if (c == '\n') {
+            --nlines;
+            if (nlines == 0) return;
+        }
+    }
+}
